9828ff1683a95eb033de1dee4ebc5597d15bf7f6
Benjamin Romero <BRomeroT@hotmail.com>
2020-08-06 20:26:12 -0500

    Core structure based on MVVM Universal architecture
    https://github.com/BRomeroT/MVVMUniversal
    
    https://youtu.be/Kz_n1iG2jqA

---
+++ Core/MPS.Core.Lib/OS/PagesKeys.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MPS.Core.Lib.OS
+{
+    public struct PagesKeys
+    {
+        public const string Login = "LoginPage";
+        public const string Crud = "CrudPage";
+        public const string Other = "OtherPage";
+    }
+}
