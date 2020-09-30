5f7785aae98682e385edd6f0310ed333c29a651e
Daniel <ogt_krake@hotmail.es>
2020-09-30 17:31:48 -0500

    Api CrearSocio y Header AppCliente

--- Core/MPS.Core.Lib/ApiSocio/SociosApi.cs
+++ Core/MPS.Core.Lib/ApiSocio/SociosApi.cs
@@ -1,8 +1,10 @@
 ﻿using MPS.Core.Lib.Helpers;
+using MPS.SharedAPIModel.Socios;
 using Sysne.Core.ApiClient;
 using System;
 using System.Collections.Generic;
 using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +12,28 @@ namespace MPS.Core.Lib.ApiSocio
 {
     public class SociosApi : WebApiClient
     {
-        public SociosApi() : base(Settings.Current.WebAPIUrl, "SolicitudesSocios") { }
+        public SociosApi() : base(Settings.Current.WebAPIUrl, "Socios") { }
 
+        public async Task<(HttpStatusCode StatusCode, bool Exito)> CrearSocioAsync(NuevoSocio nuevoSocio)
+        {
+            var res = await CallFormUrlEncoded<bool>("CrearSocio", HttpMethod.Post,
+                ("P_NOMBRE", nuevoSocio.P_NOMBRE),
+                ("P_APELLIDO_1", nuevoSocio.P_APELLIDO_1),
+                ("P_APELLIDO_2", nuevoSocio.P_APELLIDO_2),
+                ("P_FECHA_NACIMIENTO", nuevoSocio.P_FECHA_NACIMIENTO),
+                ("P_GUID_CIUDAD_OPERACIÓN", nuevoSocio.P_GUID_CIUDAD_OPERACIÓN.ToString()),
+                ("P_GUID_PAIS", nuevoSocio.P_GUID_PAIS.ToString()),
+                ("P_GUID_ESTADO_PROVINCIA", nuevoSocio.P_GUID_ESTADO_PROVINCIA.ToString()),
+                ("P_GUID_MUNICIPIO", nuevoSocio.P_GUID_MUNICIPIO.ToString()),
+                ("P_GUID_COLONIA", nuevoSocio.P_GUID_COLONIA.ToString()),
+                ("P_CALLE", nuevoSocio.P_CALLE.ToString()),
+                ("P_NO_EXT", nuevoSocio.P_NO_EXT.ToString()),
+                ("P_NO_INT", nuevoSocio.P_NO_INT.ToString()),
+                ("P_ALIAS", nuevoSocio.P_ALIAS),
+                ("P_PWD", nuevoSocio.P_PWD),
+                ("P_E_MAIL", nuevoSocio.P_E_MAIL),
+                ("P_TEL_NUMERO", nuevoSocio.P_TEL_NUMERO.ToString()));
+            return res;
+        }
     }
 }
---
+++ Core/MPS.Core.SharedAPIModel/Socios/NuevoSocio.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MPS.SharedAPIModel.Socios
+{
+    public class NuevoSocio
+    {
+        public string P_NOMBRE { get; set; }
+        public string P_APELLIDO_1 { get; set; }
+        public string P_APELLIDO_2 { get; set; }
+        public string P_FECHA_NACIMIENTO { get; set; }
+        public Guid P_GUID_SEXO { get; set; }
+        public string P_CURP { get; set; }
+        public string P_RFC { get; set; }
+        public Guid P_GUID_CIUDAD_OPERACIÓN { get; set; }
+        public Guid P_GUID_PAIS { get; set; }
+        public Guid P_GUID_CP { get; set; }
+        public Guid P_GUID_ESTADO_PROVINCIA { get; set; }
+        public Guid P_GUID_MUNICIPIO { get; set; }
+        public Guid P_GUID_COLONIA { get; set; }
+        public string P_CALLE { get; set; }
+        public int P_NO_EXT { get; set; }
+        public long P_NO_INT { get; set; }
+        public string P_REFERENCIA { get; set; }
+        public string P_ALIAS { get; set; }
+        public string P_PWD { get; set; }
+        public string P_E_MAIL { get; set; }
+        public long P_TEL_NUMERO { get; set; }
+    }
+}
