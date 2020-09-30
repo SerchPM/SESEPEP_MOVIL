4208eb28885e09b6b63e31c0033506cf0e15e6e9
Daniel <ogt_krake@hotmail.es>
2020-09-30 16:06:00 -0500

    Nombre del Socio en Header AppSocio y clase SociosApi

---
+++ Core/MPS.Core.Lib/ApiSocio/SociosApi.cs
@@ -0,0 +1,16 @@
+﻿using MPS.Core.Lib.Helpers;
+using Sysne.Core.ApiClient;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPS.Core.Lib.ApiSocio
+{
+    public class SociosApi : WebApiClient
+    {
+        public SociosApi() : base(Settings.Current.WebAPIUrl, "SolicitudesSocios") { }
+
+    }
+}
