1a6ff35ff9401afab7d682706e041bca25e7568e
Benjamin Romero <BRomeroT@hotmail.com>
2021-03-24 16:51:16 -0600

    Add Openpay ShareProject

---
+++ OpenPay/OpenpayShared/OpenpayGenericService.cs
@@ -0,0 +1,35 @@
+﻿using Openpay.Entities;
+using Openpay.Entities.Request;
+using Openpay.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Openpay
+{
+    public abstract class OpenpayGenericService
+    {
+        protected OpenpayHttpClient httpClient;
+
+        public OpenpayGenericService(string api_key, string merchant_id, bool production = false)
+        {
+            this.httpClient = new OpenpayHttpClient(api_key, merchant_id, production);
+        }
+
+		internal OpenpayGenericService(OpenpayHttpClient opHttpClient)
+        {
+            this.httpClient = opHttpClient;
+        }
+			
+        protected virtual T Get<T>(string ep)
+        {
+            return this.httpClient.Get<T>(ep);
+        }
+
+        protected List<T> List<T>(string url)
+        {
+            return this.httpClient.Get<List<T>>(url);
+        }
+    }
+}
